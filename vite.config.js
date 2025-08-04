02eb7ef38ed693ac526bfb99615d201905858e2d
Phanee <venkataphaneendra8@gmail.com>
2025-08-04 10:01:08 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'jenkinserp',  //deploy folder name but not git repo name
 })
